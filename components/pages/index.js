109e8b3a2b9601cbe63b6dc0bde9ced0bb7c71d9
Freeze-droid <andre2014521@gmail.com>
2025-04-22 22:46:11 -0400

    Initial commit

---
+++ components/pages/index.js
@@ -0,0 +1,10 @@
+import CalendarManager from '../components/CalendarManager';
+
+export default function Home() {
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Student Calendar Manager</h1>
+      <CalendarManager />
+    </main>
+  );
+}
